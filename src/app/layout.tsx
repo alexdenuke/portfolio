d5471562b1666f8c020329041b3948e8f438d742
alexdenuke <alexdenuke@gmail.com>
2026-04-23 17:12:44 +0300

    add openGraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ import "./globals.css";
 const metadataBase = new URL(
   process.env.NEXT_PUBLIC_SITE_URL ??
     process.env.NEXT_PUBLIC_APP_URL ??
+    (process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+      : undefined) ??
     (process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
       : "http://localhost:3000"),
